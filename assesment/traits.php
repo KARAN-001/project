732828093834a525f8f2312374ce1fb7a88cfe35
Karan SIngh <71657272+KARAN-001@users.noreply.github.com>
2021-01-11 19:54:59 +0530

    Update traits.php

--- assesment/traits.php
+++ assesment/traits.php
@@ -2,7 +2,9 @@
 
 <?php
 
+//Traits are used to declare methods that can be used in multiple classes. Traits can have methods and abstract methods that can be used in multiple classes, and the methods can have any access modifier (public, private, or protected).
 
+//Traits are declared with the trait keyword:
 trait trait1
 {
     public function traitmsg() {
