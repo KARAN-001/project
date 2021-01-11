438973d3c437fcedafd590271340b16fc145cc39
Karan SIngh <71657272+KARAN-001@users.noreply.github.com>
2021-01-11 19:52:18 +0530

    Update fornamespace.php

--- assesment/fornamespace.php
+++ assesment/fornamespace.php
@@ -5,6 +5,7 @@ class fornamespace{
 
     public function display()
     {
+        //This file has the definiton for the usae of namesapces
       echo "<br />My defination is present in the fornamespace.php";
     }
   }
