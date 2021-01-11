9a1749d76f36f0bafde52d701d0b6b17bbb35223
Karan SIngh <71657272+KARAN-001@users.noreply.github.com>
2021-01-11 19:53:52 +0530

    Update namespace.php

--- assesment/namespace.php
+++ assesment/namespace.php
@@ -1,4 +1,7 @@
 <?php
+// Namespaces can be used to organize the classes into two different groups while also preventing the two classes from being mixed up.
+
+
 include "fornamespace.php";
 
 $obj = new usage\fornamespace;
